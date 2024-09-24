ef1bc7e6da1d3b8c4faf77e4e0a4467350060d1a
tenugui-taro <tenu.taro@gmail.com>
2024-09-24 23:01:36 +0900

    update EMOTIONS

--- post-update-expression.ts
+++ post-update-expression.ts
@@ -1,7 +1,9 @@
 // deno run --allow-net post-update-expression.ts
 
+const EMOTIONS = ["HAPPY", "ANGRY", "SAD", "SLEEPY"];
+
 const url = "http://localhost:8080/update-expression";
-const data = { expression: "happy" };
+const data = { expression: EMOTIONS[Math.floor(Math.random() * EMOTIONS.length)] };
 
 const response = await fetch(url, {
   method: "POST",
